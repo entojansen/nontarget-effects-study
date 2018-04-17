927a32464f26bda619653f511a5a41abc533a0ad
Unknown <entojansen@gmail.com>
2018-04-16 17:02:35 -0700

    lmer

--- 2015/analysis 2015/analysis.R
+++ 2015/analysis 2015/analysis.R
@@ -14,6 +14,14 @@ SqDev = function(x) {var(x) * (length(x) - 1)}
 # Analysis 1 -------------------------------------------------------------------
 #   correspondence b/w sample weight and diversity, by sample method and order
 Analysis1 = function(raw.data) {
+  #Create folder for plots
+  dir.create("plots/mass-vs-count/scatter")
+  dir.create("plots/mass-vs-count/residual")
+  dir.create("plots/mass-vs-count/qqplot")
+  
+  name = "mass-vs-count.txt"
+  capture.output(cat("Linear Regression by sample method and order\n\n"), file = name)
+  
   #Orders that don't break the script... droplevels not removing unused orders.
   orders = c("Coleoptera",
             "Diptera",
@@ -24,19 +32,27 @@ Analysis1 = function(raw.data) {
   for (method.type in levels(raw.data$method)) {
     for (name.order in orders) {
       # Regression via linear model
-      temp.factor.x = raw.data[with(raw.data, year == 2015
+      temp.mass = raw.data[with(raw.data, year == 2015
                                     & method == method.type
                                     & order == name.order
                                     & mass > 0
                                     & family_count > 0), ]$mass
-      temp.factor.y = raw.data[with(raw.data, year == 2015
+      temp.family = raw.data[with(raw.data, year == 2015
                                     & method == method.type
                                     & order == name.order
                                     & mass > 0
                                     & family_count > 0), ]$family_count
-      temp.frame = data.frame(temp.factor.y, temp.factor.x)
+      temp.frame = data.frame(temp.family, temp.mass)
       lin.mod = lm(temp.frame)
-      print(summary(lin.mod))
+      
+      # Write linear model output to console and file
+      cat(method.type, name.order)
+      data.summary = summary(lin.mod)
+      print(data.summary)
+
+      capture.output(cat(method.type, name.order), file = name, append = TRUE)
+      capture.output(data.summary, file = name, append = TRUE)
+      
       # Make scatter plots for each order
       plot(family_count ~ mass,
            data = raw.data[with(raw.data, year == 2015
@@ -50,7 +66,7 @@ Analysis1 = function(raw.data) {
            )
       abline(lin.mod, col = "blue")
       dev.copy(pdf,
-               paste("plots/2015",
+               paste("plots/mass-vs-count/scatter/2015",
                      method.type,
                      name.order,
                      "mass-vs-count.pdf",
@@ -59,14 +75,72 @@ Analysis1 = function(raw.data) {
                height = 3
                )
       dev.off()
+      
+      # Plot the residuals vs. the fitted values
+      plot(residuals(lin.mod) ~ fitted(lin.mod),
+           data = temp.frame,
+           xlab = "fitted family count",
+           ylab = "residual family count",
+           main = NULL
+      )
+      dev.copy(pdf,
+               paste("plots/mass-vs-count/residual/2015",
+                     method.type,
+                     name.order,
+                     "residual.pdf",
+                     sep = "_"),
+               width = 3,
+               height = 3
+      )
+      dev.off()
+      
+      # Make a normal probability plot of the residuals
+      qqnorm(lin.mod$residuals,
+             ylab = "Residual Quantiles",
+             main = NULL
+      )
+      qqline(lin.mod$residuals,
+             probs = c(0.25, 0.75),
+             col = 'blue'
+      )
+      
+      dev.copy(pdf,
+               paste("plots/mass-vs-count/qqplot/2015",
+                     method.type,
+                     name.order,
+                     "qqplot.pdf",
+                     sep = "_"),
+               width = 3,
+               height = 3
+      )
+      dev.off()
     }
   }
 }
 
 # Analysis 2 -------------------------------------------------------------------
-#   correspondence b/w sample weight and diversity per order, by sample method
+#   linear mixed model: sample weight and diversity per order and sample method
 Analysis2 = function(raw.data) {
+  # Mixed linear model with mass as fixed effect and order and method as random
+  mixed.lmer = lmer(family_count ~ mass + (1 | order) + (1 | method),
+              data = raw.data[with(raw.data, mass > 0 & family_count > 0), ], family = poisson)
   
+  print(summary(mixed.lmer))
+  
+  plot(fitted(mixed.lmer),
+       residuals(mixed.lmer),
+       xlab = "fitted family count",
+       ylab = "residual family count"
+  )
+  
+  qqnorm(residuals(mixed.lmer),
+         ylab = "Residual Quantiles",
+         main = NULL
+  )
+  qqline(residuals(mixed.lmer),
+         probs = c(0.25, 0.75),
+         col = 'blue'
+  )
 }
 
 # Analysis 3 -------------------------------------------------------------------
@@ -102,6 +176,7 @@ main = function() {
   #Load gplots for plotCI
   library(gplots)
   library(car)
+  library(lme4)
   
   #Create folder for plots
   dir.create("plots")
@@ -115,8 +190,9 @@ main = function() {
   
   readline(prompt = "first analysis: press enter to continue.")
   Analysis1(raw.data)
-  # readline(prompt = "second analysis: press enter to continue.")
-  # Analysis2(raw.data)
+  
+  readline(prompt = "second analysis: press enter to continue.")
+  Analysis2(raw.data)
   # 
   # readline(prompt = "third analysis: press enter to continue.")
   # Analysis3(raw.data)
