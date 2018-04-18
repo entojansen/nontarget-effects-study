0a9263b60f70a17a107883dc00912b1c9e2c52ab
Unknown <entojansen@gmail.com>
2018-04-17 23:38:50 -0700

    all order/method data analyzed, begin bulk

--- 2015/analysis 2015/analysis.R
+++ 2015/analysis 2015/analysis.R
@@ -166,7 +166,6 @@ Analysis2 = function(raw.data) {
   
   capture.output(summary(mixed.lmer.poisson), file = name, append = TRUE)
   capture.output(summary(mixed.lmer.nb), file = name, append = TRUE)
-  capture.output(summary(mixed.lmer.nb), file = name, append = TRUE)
   capture.output(lrtest(mixed.lmer.poisson, mixed.lmer.nb),
                  file = name, append = TRUE)
   capture.output(lrtest(mixed.lmer.nb,
@@ -206,16 +205,316 @@ Analysis2 = function(raw.data) {
 }
 
 # Analysis 3 -------------------------------------------------------------------
-#   comparisons between blocks for bulk diversity and sample weight
-#   all comparisons (here and below) by sample method and in bulk
+#   comparisons between blocks for diversity and weight of individual orders
 Analysis3 = function(raw.data) {
+  dir.create("plots/blocks")
+  dir.create("plots/blocks/tukeyHSD")
+  dir.create("plots/blocks/tukeyHSD/mass")
+  dir.create("plots/blocks/tukeyHSD/family")
+  dir.create("plots/blocks/interaction")
+  dir.create("plots/blocks/interaction/mass")
+  dir.create("plots/blocks/interaction/family")
   
+  name = "blocks.txt"
+  capture.output(cat("Analyses of Variance (order, method) \n\n"), file = name)
+  
+  #Orders that don't break the script... droplevels not removing unused orders.
+  orders = c("Coleoptera",
+             "Diptera",
+             "Hemiptera",
+             "Hymenoptera",
+             "Lepidoptera",
+             "Orthoptera",
+             "Neuroptera")
+  
+  raw.data.2015 = raw.data[raw.data$year == 2015, ]
+  
+  for (method.type in levels(raw.data.2015$method)) {
+    for (name.order in orders) {
+      capture.output(cat(name.order, method.type, "\n\n"),
+                     file = name, append = TRUE)
+      temp.data = raw.data.2015[with(raw.data.2015,
+                                method == method.type 
+                                & order == name.order), ]
+      temp.data$event = factor(temp.data$event)
+      mass.model = lm(mass ~ block * event, temp.data)
+      family.model = lm(family_count ~ block * event, temp.data)
+      mass.aov = aov(mass.model)
+      family.aov = aov(family.model)
+      
+      capture.output(cat("sample mass", "\n\n"), file = name, append = TRUE)
+      capture.output(summary(mass.aov), file = name, append = TRUE)
+      capture.output(leveneTest(mass.model), file = name, append = TRUE)
+      capture.output(shapiro.test(residuals(mass.model)),
+                     file = name, append = TRUE)
+      capture.output(TukeyHSD(mass.aov), file = name, append = TRUE)
+      
+      par(mar = c(4, 4, 2, 0.6) + 0.1)
+      plot(TukeyHSD(mass.aov))
+      dev.copy(pdf,
+               paste("plots/blocks/tukeyHSD/mass/mass",
+                     method.type,
+                     name.order,
+                     "anova.pdf",
+                     sep = "_"),
+               width = 7.5,
+               height = 200
+      )
+      dev.off()
+      par(mar = c(4, 4, 1.1, 0.6) + 0.1)
+      
+      mass.anova = anova(mass.model)
+      MS.res = mass.anova$Mean[4]
+      nu = mass.anova$Df[4]
+      means = tapply(temp.data$mass,
+                     list(temp.data$block, temp.data$event),
+                     mean)
+      alpha = 0.05
+      n = tapply(temp.data$mass,
+                 list(temp.data$block, temp.data$event),
+                 length)
+      se = sqrt(MS.res / n)
+      t.critical = qt(1 - alpha / 2, nu)
+      inter = t.critical * se
+      
+      plotCI(x = 1:5,
+             y = means[1,],
+             uiw = inter[1,],
+             type = "b",
+             xaxt = "n",
+             col = "red",
+             xlab = "Sample Week",
+             ylab = "Mean Order Mass",
+             ylim = c(-1, 8),
+             xlim = c(1, 6),
+             main = paste(name.order, " (", method.type, ")", sep = "")
+      )
+      plotCI(x = 1:5 + 0.05,
+             y = means[2,],
+             uiw = inter[2,],
+             type = "b",
+             xaxt = "n",
+             col = "blue",
+             add = TRUE
+      )
+      plotCI(x = 1:5 + 0.1,
+             y = means[3,],
+             uiw = inter[3,],
+             type = "b",
+             xaxt = "n",
+             col = "green",
+             add = TRUE
+      )
+      plotCI(x = 1:5 + 0.15,
+             y = means[4,],
+             uiw = inter[4,],
+             type = "b",
+             xaxt = "n",
+             col = "black",
+             add = TRUE
+      )
+      axis(1, at = 1:5, labels = c(1, 2, 3, 4, 5))
+      legend(x = 5.25,
+             y = 0,
+             yjust = 0,
+             lty = 1,
+             col = c("red", "blue", "green", "black"),
+             legend = c("1---", "2---", "3---", "UTC       "),
+             bty = "o"
+      )
+      dev.copy(pdf,
+               paste("plots/blocks/interaction/mass/mass",
+                     method.type,
+                     name.order,
+                     "interplot.pdf",
+                     sep = "_"),
+               width = 6,
+               height = 3
+      )
+      dev.off()
+      
+      capture.output(cat("sample families", "\n\n"), file = name, append = TRUE)
+      capture.output(summary(family.aov), file = name, append = TRUE)
+      capture.output(leveneTest(family.model), file = name, append = TRUE)
+      capture.output(shapiro.test(residuals(family.model)),
+                     file = name, append = TRUE)
+      capture.output(TukeyHSD(family.aov), file = name, append = TRUE)
+      
+      par(mar = c(4, 4, 2, 0.6) + 0.1)
+      plot(TukeyHSD(family.aov))
+      dev.copy(pdf,
+               paste("plots/blocks/tukeyHSD/family/family",
+                     method.type,
+                     name.order,
+                     "anova.pdf",
+                     sep = "_"),
+               width = 7.5,
+               height = 200
+      )
+      dev.off()
+      par(mar = c(4, 4, 1.1, 0.6) + 0.1)
+      
+      family.anova = anova(family.model)
+      MS.res = family.anova$Mean[4]
+      nu = family.anova$Df[4]
+      means = tapply(temp.data$family_count,
+                     list(temp.data$block, temp.data$event),
+                     mean)
+      alpha = 0.05
+      n = tapply(temp.data$family_count,
+                 list(temp.data$block, temp.data$event),
+                 length)
+      se = sqrt(MS.res / n)
+      t.critical = qt(1 - alpha / 2, nu)
+      inter = t.critical * se
+      
+      plotCI(x = 1:5,
+             y = means[1,],
+             uiw = inter[1,],
+             type = "b",
+             xaxt = "n",
+             col = "red",
+             xlab = "Sample Week",
+             ylab = "Mean Family Count",
+             ylim = c(-1, 25),
+             xlim = c(1, 6),
+             main = paste(name.order, " (", method.type, ")", sep = "")
+      )
+      plotCI(x = 1:5 + 0.05,
+             y = means[2,],
+             uiw = inter[2,],
+             type = "b",
+             xaxt = "n",
+             col = "blue",
+             add = TRUE
+      )
+      plotCI(x = 1:5 + 0.1,
+             y = means[3,],
+             uiw = inter[3,],
+             type = "b",
+             xaxt = "n",
+             col = "green",
+             add = TRUE
+      )
+      plotCI(x = 1:5 + 0.15,
+             y = means[4,],
+             uiw = inter[4,],
+             type = "b",
+             xaxt = "n",
+             col = "black",
+             add = TRUE
+      )
+      axis(1, at = 1:5, labels = c(1, 2, 3, 4, 5))
+      legend(x = 5.25,
+             y = 0,
+             yjust = 0,
+             lty = 1,
+             col = c("red", "blue", "green", "black"),
+             legend = c("1---", "2---", "3---", "UTC       "),
+             bty = "o"
+      )
+      dev.copy(pdf,
+               paste("plots/blocks/interaction/family/family",
+                     method.type,
+                     name.order,
+                     "interplot.pdf",
+                     sep = "_"),
+               width = 6,
+               height = 3
+      )
+      dev.off()
+    }
+  }
 }
 
 # Analysis 4 -------------------------------------------------------------------
-#   comparisons between blocks for diversity and weight of individual orders
+#   comparisons between blocks for bulk diversity and sample weight
+#   all comparisons (here and below) by sample method and in bulk
 Analysis4 = function(raw.data) {
+  dir.create("plots/bulk")
   
+  name = "bulk.txt"
+  capture.output(cat("Analyses of Variance (bulk) \n\n"), file = name)
+  
+  raw.data.2015 = raw.data[raw.data$year == 2015, ]
+  raw.data.2015$event = factor(raw.data.2015$event)
+  
+  bulk.data = aggregate(formula = (cbind(mass, family_count) 
+                           ~ site + event + block + method),
+                data = raw.data.2015,
+                FUN = sum
+  )
+  
+  bulk.lm = lm(family_count ~ mass,
+               data = bulk.data[with(bulk.data, mass > 0 
+                                     & family_count > 0), ])
+  
+  plot(family_count ~ mass,
+       data = bulk.data[with(bulk.data, mass > 0 & family_count > 0), ],
+       xlab = "sample mass (g)",
+       ylab = "number of families",
+       main = "Bulk (by site and event)"
+  )
+  abline(bulk.lm, col = "blue")
+  dev.copy(pdf,
+           paste("plots/bulk/2015_bulk_scatter.pdf",
+                 sep = "_"),
+           width = 3,
+           height = 3
+  )
+  dev.off()
+  
+  # TODO Break down by method, then go to glmer's and MANOVA
+  
+  mixed.lmer.poisson = glmer(family_count ~ mass + (1 | method),
+                             data = bulk.data[with(bulk.data, mass > 0 
+                                                  & family_count > 0), ],
+                             family = poisson)
+  
+  mixed.lmer.nb = glmer.nb(family_count ~ mass + (1 | method),
+                           data = bulk.data[with(bulk.data, mass > 0 
+                                                 & family_count > 0), ],
+                           family = negative.binomial)
+  
+  print(summary(bulk.lm))
+  print(summary(mixed.lmer.poisson))
+  print(summary(mixed.lmer.nb))
+  print(lrtest(mixed.lmer.poisson, mixed.lmer.nb))
+  
+  capture.output(summary(bulk.lm), file = name, append = TRUE)
+  capture.output(summary(mixed.lmer.poisson), file = name, append = TRUE)
+  capture.output(summary(mixed.lmer.nb), file = name, append = TRUE)
+  capture.output(lrtest(mixed.lmer.poisson, mixed.lmer.nb),
+                 file = name, append = TRUE)
+  
+  plot(fitted(mixed.lmer.nb),
+       residuals(mixed.lmer.nb),
+       xlab = "fitted family count",
+       ylab = "residual family count"
+  )
+  dev.copy(pdf,
+           paste("plots/bulk/2015_bulk_residual.pdf",
+                 sep = "_"),
+           width = 3,
+           height = 3
+  )
+  dev.off()
+  
+  qqnorm(residuals(mixed.lmer.nb),
+         ylab = "Residual Quantiles",
+         main = NULL
+  )
+  qqline(residuals(mixed.lmer.nb),
+         probs = c(0.25, 0.75),
+         col = 'blue'
+  )
+  dev.copy(pdf,
+           paste("plots/bulk/2015_bulk_qqplot.pdf",
+                 sep = "_"),
+           width = 3,
+           height = 3
+  )
+  dev.off()
 }
 
 # Analysis 5 -------------------------------------------------------------------
@@ -239,7 +538,6 @@ main = function() {
   library(gplots)
   library(car)
   library(lme4)
-  library(qcc)
   library(lmtest)
   
   #Create folder for plots
@@ -254,15 +552,15 @@ main = function() {
   
   # readline(prompt = "first analysis: press enter to continue.")
   # Analysis1(raw.data)
-  
-  readline(prompt = "second analysis: press enter to continue.")
-  Analysis2(raw.data)
   # 
+  # readline(prompt = "second analysis: press enter to continue.")
+  # Analysis2(raw.data)
+  # # 
   # readline(prompt = "third analysis: press enter to continue.")
   # Analysis3(raw.data)
-  # 
-  # readline(prompt = "fourth analysis: press enter to continue.")
-  # Analysis4(raw.data)
+  # # 
+  readline(prompt = "fourth analysis: press enter to continue.")
+  Analysis4(raw.data)
   # 
   # readline(prompt = "fifth analysis: press enter to continue.")
   # Analysis5(raw.data)
